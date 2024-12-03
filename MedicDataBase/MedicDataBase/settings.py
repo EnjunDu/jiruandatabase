44859776dcc7df420e9a55ef441967cbb35c139b
ckt1i <changqian.zheng@gmail.com>
2024-12-03 19:16:55 +0800

    modified:   MedicDataBase/MedicDataBase/settings.py

--- MedicDataBase/MedicDataBase/settings.py
+++ MedicDataBase/MedicDataBase/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'medicaldata',
         'USER': 'root',
-        'PASSWORD': '2enDSG9byAHJvfx',
+        'PASSWORD': 'yourpassword',
         'HOST': 'localhost',
         'PORT': '3306',
     }
